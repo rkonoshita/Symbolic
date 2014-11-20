9f1c68ae7c3cbce7b4f992d14c3d09b9f3a2b66d
rkonoshita <rkono1014@gmail.com>
2014-11-20 19:01:12 +0900

    master

--- src/parser/ASTVisitor.scala
+++ src/parser/ASTVisitor.scala
@@ -5,8 +5,12 @@ package parser
  */
 class ASTVisitor {
 
-  def visit(ast: AST): Unit = {
-
-  }
+//  def visit(ast: AST): Int = {
+//    case Add(left, right) =>
+//      (left, right) match {
+//        case (l: Imm, r: RegByte) => Array(0x80 | visit(r), visit(l))
+//        case (l: RegByte, r: RegByte) => Array(0x08, (visit(l) << 4) | visit(r))
+//      }
+//  }
 
 }
